314313c0dc3faf8a99031bd355dab339f2bba251
Mishel Morales <mmejiam1825@gmail.com>
2025-06-15 22:40:12 -0600

    Se arreglo la funcion de ultimo turno y se borraron lineas de codigo que estaban de mas

--- FIFO.js
+++ FIFO.js
@@ -10,8 +10,7 @@ const mostrarProximoTurno = document.querySelector('#exampleFormControlInput3');
 
 let agregar = '';
 let paciente;
-let tabla;
-let tablaVacia = 0;
+let tablaVacia = true;
 let numeroPaciente = 0;
 
 class NodePaciente{ // Se crea el nodo paciente con los datos proporcionados;
@@ -34,9 +33,11 @@ class Queue{ // Se agrega a la cola;
 
         const paciente = new NodePaciente(nombre, edad, sintoma);
         this.#pacientesEspera += `Nombre del Paciente: ${paciente.Nombre}. Edad del Paciente: ${paciente.Edad} años. Sintoma Principal del Paciente: ${paciente.SintomaPrincipal}. \n`
-        if(this.length == 0){
+        
+        if(this.length == 0 || tablaVacia==true){
             this.Primero = paciente;
             this.Ultimo = paciente;
+
         } else{
             this.Ultimo.next = paciente;
             this.Ultimo = paciente;
@@ -54,16 +55,16 @@ class Queue{ // Se agrega a la cola;
             this.Primero = this.Primero.next;
             this.Ultimo = null
         }
-        
+
         this.length--;
         return this;
     }
     peek(){ // Muestra al primer paciente de la cola;
         return this.Primero;
     }
-    isEmpty(){ // Verifica si no hay pacientes;
+    isEmpty(val){ // Verifica si no hay pacientes;
 
-        if(this.length != 0){
+        if(this.length != 0 && val != 0){
             return `Hay ${this.length} pacientes`;
         } else{
             return 'No hay pacientes';
@@ -89,29 +90,22 @@ const crearFila = (nombre, edad, sintomas, numeroPaciente) => {
 }
 
 const agregarPaciente = () => {
-
-    paciente = newPaciente.peek();
     
     newPaciente.enqueue(nombrePaciente.value, edadPaciente.value, sintomasPaciente.value);
-
-    let filas = insertarDatosTabla.querySelectorAll('tr').length+1;
+    paciente = newPaciente.peek();
 
     numeroPaciente++;
     
     agregar = crearFila(nombrePaciente.value, edadPaciente.value, sintomasPaciente.value, numeroPaciente);
     insertarDatosTabla.innerHTML += agregar;
     
-    console.log(agregar)
     paciente = newPaciente.peek();
     mostrarProximoTurno.value = `  Proximo Turno: ${paciente.Nombre}`;
-    
-    console.log(newPaciente)
 
-    tablaVacia = 0;
+    tablaVacia = false;
     nombrePaciente.value = '';
     edadPaciente.value = '';
-    sintomasPaciente.value = '';
-    
+    sintomasPaciente.value = '';   
 }
 
 const decir = (texto) => { // Para que diga el nombre del paciente en el turno actual;
@@ -122,8 +116,7 @@ const decir = (texto) => { // Para que diga el nombre del paciente en el turno a
 
 const agregarDatos = () => {
 
-    paciente = newPaciente.peek();
-    if(tablaVacia == 0){
+    if(tablaVacia == false){
         mostrarNombrePaciente.textContent = paciente.Nombre;
         mostrarEdadSintoma.textContent = `${paciente.Edad} - ${paciente.SintomaPrincipal}`;
         decir(paciente.Nombre);
@@ -136,23 +129,19 @@ const agregarDatos = () => {
     }
     
     if(paciente.next != null){
-        
         paciente = newPaciente.dequeue();
         paciente = newPaciente.peek();
         mostrarProximoTurno.value = `  Proximo Turno: ${paciente.Nombre}`;
 
     } else{
-        tablaVacia++;
-        mostrarProximoTurno.value = '  Proximo Turno: No hay más pacientes'
+        tablaVacia = true;
+        mostrarProximoTurno.value = `  Proximo Turno: ${paciente = newPaciente.isEmpty(0)}`;
     }
 
     let fila = insertarDatosTabla.querySelector('tr');
-    insertarDatosTabla.removeChild(fila);
-
+    numeroPaciente != 0 ? insertarDatosTabla.removeChild(fila) : null;
 }
 
 btnAgregarCola.addEventListener('click', agregarPaciente);
 btnAtenderPaciente.addEventListener('click', agregarDatos);
-
-console.log(newPaciente)
 
